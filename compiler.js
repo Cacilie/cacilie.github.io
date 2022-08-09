91a0f0f2ca7c1b566ded8022cb5d6708c5a76fb7
camara <camara@camera.local>
2022-08-08 20:17:57 -0400

    add initial configuration for development

---
+++ compiler.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const pug = require('pug');
+
+const compiledFunction = pug.compileFile('./templates/home.pug');
+
+
+fs.writeFileSync("./build/index.html", compiledFunction());
